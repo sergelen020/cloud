011a10c44d9944712999af5db1746de47a459b56
sergelen <sergelen020@gmail.com>
2021-06-15 11:54:43 +0800

    h

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const qrcode = require('qrcode')
 const axios = require('axios')
 const functions = require('firebase-functions');
-const cors = require('cors')({origin: true})
+const cors = require('cors')({origin: true});
 
 const admin = require('firebase-admin')
 admin.initializeApp()
